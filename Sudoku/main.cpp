833c617fbcfc229975922b2d5659dd93be5a1b83
mikarguelles <ekimargs@gmail.com>
2020-09-29 12:27:14 +0200

    Initial Commit

---
+++ Sudoku/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  Sudoku
+//
+//  Created by Miguel Argüelles on 2020-09-29.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
